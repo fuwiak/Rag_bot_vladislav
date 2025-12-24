ee3b0becdcaa85b67067f5f756eb33b6cfd4b7eb
fuwiak <poczta130@gmail.com>
2025-12-24 18:39:08 +0300

    Add Alembic migration for bot_is_active field
    - Create migration to add bot_is_active column to projects table
    - Default value is 'false' for existing projects
    - Migration can be run with: alembic upgrade head

---
+++ backend/alembic/versions/2025_12_24_add_bot_is_active.py
@@ -0,0 +1,27 @@
+"""Add bot_is_active field to projects
+
+Revision ID: 2025_12_24_bot_is_active
+Revises: 2025_12_13_1637
+Create Date: 2025-12-24 15:30:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2025_12_24_bot_is_active'
+down_revision = '2025_12_13_1637'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Добавляем поле bot_is_active в таблицу projects
+    op.add_column('projects', sa.Column('bot_is_active', sa.String(length=10), nullable=False, server_default='false'))
+
+
+def downgrade():
+    # Удаляем поле bot_is_active
+    op.drop_column('projects', 'bot_is_active')
+
